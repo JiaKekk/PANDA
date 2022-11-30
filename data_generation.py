2adf35fc135c93f2dfc091287d4dc528784aef87
JiaKekk <117530883+JiaKekk@users.noreply.github.com>
2022-11-30 14:51:51 +0800

    lalala

---
+++ data_generation.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import torch
+torch.set_default_tensor_type(torch.FloatTensor)
+from torch.autograd import Variable
+from torch.nn import functional as F
+from torchmetrics import Accuracy, Recall, Precision, Specificity, ConfusionMatrix
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+from sklearn.metrics import confusion_matrix
+
+from sklearn.metrics import precision_recall_curve, average_precision_score,roc_curve, auc, precision_score, recall_score, f1_score, confusion_matrix, accuracy_score
+
+import numpy as np
+from IPython.core.interactiveshell import InteractiveShell
+InteractiveShell.ast_node_interactivity = "all"
+
+import matplotlib.pyplot as plt
+from collections import Counter
+import random
+import math
+import torch.optim as optim
+from tabulate import tabulate
+from ray import tune
+
+from fractions import Fraction
+
+
+# In[ ]:
+
+
+# sigmoid
+def sigmoid(X):
+    return .5 * (1 + np.tanh(.5 * X))
+
+
+# In[ ]:
+
+
+def setup_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.backends.cudnn.deterministic = True
+
+
+# In[ ]:
+
+
+def cubic_root(x):
+    return math.copysign(math.pow(abs(x), 1.0/3.0), x)
+
+
+# In[ ]:
+
+
+#linear
+def generate_data_linear(corval,beta,n,p):
+    mean=np.zeros(p)
+    sigma=np.array([[corval**abs(i-j) for i in range(p)] for j in range(p)])
+    x=torch.tensor(np.random.multivariate_normal(mean=mean,cov=sigma,size=n))
+    Pi_test= sigmoid(x@beta)
+    y=np.random.binomial(1,Pi_test.ravel(),n)
+    x=x.to(torch.float32)
+    #y=torch.tensor(y,dtype=torch.double)
+    y=torch.tensor(y,dtype=torch.float)
+    #y=y.to(torch.float32)
+    return x,y
+
+
+# In[ ]:
+
+
+def sigmoid(X):
+    return .5 * (1 + np.tanh(.5 * X))
+
+
+# In[ ]:
+
+
+#nolinear
+def generate_data_nolinear(corval,beta,n,p):
+    mean=np.zeros(p)
+    sigma=np.array([[corval**abs(i-j) for i in range(p)] for j in range(p)])
+    x_pre=torch.tensor(np.random.multivariate_normal(mean=mean,cov=sigma,size=n))
+    x=torch.clone(x_pre)
+    
+    x[:,sin_index]=np.sin(4*pi*x_pre[:,sin_index])
+    x[:,exp_index]=x_pre[:,exp_index]*x_pre[:,exp_index]*x_pre[:,exp_index]
+    
+    Pi_test= sigmoid(x@beta)
+    y=np.random.binomial(1,Pi_test.ravel(),n)
+    x=x.to(torch.float32)
+    y=torch.tensor(y,dtype=torch.float)
+    return x,y
+
+
+# In[ ]:
+
---
+++ evaluation.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import torch
+torch.set_default_tensor_type(torch.FloatTensor)
+from torch.autograd import Variable
+from torch.nn import functional as F
+from torchmetrics import Accuracy, Recall, Precision, Specificity, ConfusionMatrix
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+from sklearn.metrics import confusion_matrix
+
+from sklearn.metrics import precision_recall_curve, average_precision_score,roc_curve, auc, precision_score, recall_score, f1_score, confusion_matrix, accuracy_score
+
+import numpy as np
+from IPython.core.interactiveshell import InteractiveShell
+InteractiveShell.ast_node_interactivity = "all"
+
+import matplotlib.pyplot as plt
+from collections import Counter
+import random
+import math
+import torch.optim as optim
+from tabulate import tabulate
+from ray import tune
+
+from fractions import Fraction
+
+
+# In[ ]:
+def cubic_root(x):
+    return math.copysign(math.pow(abs(x), 1.0/3.0), x)
+
+
+# variable selection eveluation
+def variable_selection(threshold,model_1_weight,model_2_weight,model_3_weight,beta_1,p):
+    mcl_w=np.zeros(p)
+
+    for j in range(p):
+        mcl_w[j]=cubic_root(model_1_weight[0,j]*model_2_weight[0,j]*model_3_weight[0,j])
+
+    max_mcl_w=max(abs(mcl_w))
+    for j in range(p):
+        if (abs(mcl_w[j]))<=threshold*max_mcl_w:
+            model_1_weight[0,j]=0
+            model_2_weight[0,j]=0
+            model_3_weight[0,j]=0
+        else:
+            model_1_weight[0,j]=model_1_weight[0,j]
+            model_2_weight[0,j]=model_2_weight[0,j]
+            model_3_weight[0,j]=model_3_weight[0,j]
+    
+    index=np.nonzero(model_1_weight)[1]
+
+    
+    beta=np.array(beta_1)
+    trueindex=np.where(beta!=0)[0]
+    trueindex=trueindex.tolist()
+    trueindex=set(trueindex)
+    index=np.nonzero(model_1_weight)[1]
+    index=index.tolist()
+    index=set(index)
+    
+    TP=len(trueindex.intersection(index))
+    FP=len(index.difference(trueindex))
+    FN=len(trueindex)-TP
+    TN=p-len(trueindex)-FP
+    
+    return index,TP,FP,FN,TN
+
+
